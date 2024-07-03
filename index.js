e68aebd8d65e1a9c8d77eea56ec739eea103994d
Victor752-byte <obiohavictor20@gmail.com>
2024-07-02 18:41:50 -0700

    completed my stage one task

---
+++ index.js
@@ -0,0 +1,13 @@
+const d = new Date();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const day = d.getDay();
+let mark;
+function ampm(){
+    if(d.getHours() < 11){
+        return "am"
+    }
+    return "pm"
+}
+
+document.getElementById("currentTimeUTC").innerHTML=d.getHours() + ':' + d.getMinutes() + ampm();
+document.getElementById("currentDay").innerHTML= days[day];
